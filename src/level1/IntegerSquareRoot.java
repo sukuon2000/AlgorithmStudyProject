70aa18b6cd3a925509138f9722c48f2c8b13a901
sukuon2000 <kaiosuku@naver.com>
2023-02-02 23:51:51 +0900

    2023 02 02 algorithm study commit

---
+++ src/level1/IntegerSquareRoot.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class IntegerSquareRoot {
+
+    public long solution(long n) {
+        Double sqrt = Math.sqrt(n);
+
+        if(sqrt == sqrt.intValue()) {
+            return (long) Math.pow(sqrt + 1, 2);
+        } else return -1;
+    }
+}
